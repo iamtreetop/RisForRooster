f45e8313ace1ea559591054fad663ae2574505b1
iamtreetop <trivta@gmail.com>
2021-01-07 18:29:56 -0800

    Add header component and logo

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/logo.png'
+
+import './header.styles.scss';
+
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <img src={Logo} className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/about-us'>
+        OUR STORY
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
