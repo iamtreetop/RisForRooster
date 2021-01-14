584c7bf196bd7097263c5a9bb6d535fb7492dde2
iamtreetop <trivta@gmail.com>
2021-01-13 20:41:10 -0800

    Add persistance to applciation

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "../redux/user/user.reducer";
 import cartReducer from "../redux/cart/cart.reducer";
 import modalReducer from "../redux/modal/modal.reducer";
 
-export default combineReducers ({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   modal: modalReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
