e9be58733746962ec0be95eaa126aac186fb45a7
iamtreetop <trivta@gmail.com>
2021-01-17 13:06:05 -0800

    Add sticky nav hooks to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,33 +15,60 @@ import Logo from '../../assets/logo.png'
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden, openModal }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <img src={Logo} className='logo' alt="" />
-    </Link>
-    {/* <Search> */}
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+const Header = ({ currentUser, hidden, openModal }) => {
+  
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const header = document.querySelector('.header');
+    const topOfHeader = header.offsetTop;
+    const offset = window.scrollY;
+
+    if (offset > topOfHeader) {
+      document.body.style.paddingTop = header.offsetHeight + 25 + 'px';
+      setScrolled(true);
+    } else {
+      document.body.style.paddingTop = 0;
+      setScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  });
+
+  let headerClass = ['header']
+  if (scrolled) {
+    headerClass.push('scrolled')
+  }
+
+  return (
+    <div className={headerClass.join(" ")}>
+      <Link className='logo-container' to='/'>
+        <img src={Logo} className='logo' alt="" />
       </Link>
-      <Link className='option' to='/about-us'>
-        OUR STORY
-      </Link>
-      {currentUser ? (
-        <div className='option'onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <div className='option' onClick={openModal}>
-          SIGN IN
-        </div>
-      )}
-      <CartIcon />
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/about-us'>
+          OUR STORY
+        </Link>
+        {currentUser ? (
+          <div className='option'onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <div className='option' onClick={openModal}>
+            SIGN IN
+          </div>
+        )}
+        <CartIcon />
+      </div>
+      {hidden ? null : <CartDropdown />}
     </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
-);
+  )
+};
 
 const mapSTP = createStructuredSelector ({
   currentUser: selectCurrentUser,
